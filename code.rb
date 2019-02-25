341525f2b4d37e9cabe6c10d2832e37f0b320d4e
Joan Indiana Lyness <45588032+joanrig@users.noreply.github.com>
2019-02-24 23:02:26 -0500

    assigned survivors/ pts to each side

--- code.rb
+++ code.rb
@@ -3,12 +3,16 @@ def alphabet_war(fight) #"z*z*z*zs"
   sides= Hash.new
   sides[:right] = {"m":4, "q":3, "d":2, "z":1}
   sides[:left] = {"w":4, "p":3, "b":2, "s":1}
-  
+
   if fight.length == 1
-    winner = sides.select {|k| k.values.include?(fight.to_sym)}
-    puts "#{winner.to_s.upcase} + side wins!"
-  else
+    if sides[:right].keys.include?(fight)
+      winner = "Right"
+    if sides[:left].keys.include?(fight)
+      winner = "Left"
+    end
+    winner + " side wins!"
   
+  else 
     #find all bomb spots by index
     bomb_spot = (0 ... fight.length).find_all {|i| fight[i] == "*"} # => [1, 3, 5]
   
@@ -21,11 +25,32 @@ def alphabet_war(fight) #"z*z*z*zs"
       else
         fight[spot-1...spot+1] = " "
       end 
-    end 
+    end
+      #=>" z  s"
     
+    #assign survivors/points to their sides
+    right_pts = []
+    left_pts = []
+      
+    fight.scan(/./).each do |survivor|
+      sides[:right].each do |k,v|
+        if survivor.to_sym == k
+          right_pts<< v.to_i
+        end
+      end
+    end
+      
+    fight.scan(/./).each do |survivor|
+      sides[:left].each do |k,v|
+        if survivor.to_sym == k
+          left_pts<< v.to_i
+        end
+      end
+    end
+    
+    return "Let's fight again!"  if right_pts.sum == left_pts.sum
+    
+    right_pts.sum > left_pts.sum ? "Right side wins!" : "Left side wins!"  
   end #fight.length
 end #method
-  
- 
-  
-  
+end
