b00b4a359725706b1b5a65f3150641bdf18865e8
Joan Indiana Lyness <45588032+joanrig@users.noreply.github.com>
2019-02-24 22:29:10 -0500

    fixed sides hash

--- code.rb
+++ code.rb
@@ -1,9 +1,8 @@
 def alphabet_war(fight) #"z*z*z*zs"
   
-  sides =[
-  "right" => ["m":4, "q":3, "d":2, "z":1],
-  "left" => ["w":4, "p":3, "b":2, "s":1]
-  ]
+  sides= Hash.new
+  sides[:right] = {"m":4, "q":3, "d":2, "z":1}
+  sides[:left] = {"w":4, "p":3, "b":2, "s":1}
   
   if fight.length == 1
     winner = sides.select {|k| k.values.include?(fight.to_sym)}
